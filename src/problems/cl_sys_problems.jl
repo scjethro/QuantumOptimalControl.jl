87a505df42070c523e169b021ce28c388cbc8d9f
Alastair <alastair@nvision-imaging.com>
2019-09-25 10:07:32 +0200

    changing the ClosedStateTransfer struct a little to accept AbstractArray, makes it easier to use sparsematrices in future

--- src/problems/cl_sys_problems.jl
+++ src/problems/cl_sys_problems.jl
@@ -20,18 +20,18 @@ struct ClosedStateTransfer <: ClosedProblem
     # this allows you to think about doing optimisation over an ensemble of systems.
 
     # store for the Drift Hamiltonian, can either be a 2D Complex array or a function
-    drift_ham::Union{Array{Complex, 2}, Function}
+    drift_ham::Union{AbstractArray{Complex,2},Function}
     # store for the Conrtol Hamiltonian, can either be a list of Hamiltonians or some functions
     # since we're not going for a general solver here but something specific to pulse sequences 
     # at the moment we don't need to take care of the case where the user supplies some coefficients
     # to the controls, since the coefficients will be taken from the pulse directly 
-    control_hams::Union{Array{Array{Complex, 2}, 1}, Function}
+    control_hams::Union{AbstractArray{AbstractArray{Complex,2},1},Function}
     # want to store the states as density matrices for convenience, is there a reason to store psi?
-    initial_state::Array{Complex, 2}
-    final_state::Array{Complex, 2}
+    initial_state::AbstractArray{Complex,2}
+    final_state::AbstractArray{Complex,2}
     # each problem can have either one or a list of optimal pulses associated with it, 
     # we can then update the amplitude and phases of these as we go?
-    optimal_pulse::OptimalPulse
+    optimal_pulse::AbstractArray{OptimalPulse,1}
 
     function ClosedStateTransfer(dh, ch, is, fs)
         op = OptimalPulse([], [], 0.0) # just a generic optimal pulse with no initial values
